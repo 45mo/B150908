d70ba886dacacf4e1816924c79be8466fcedd3c1
B15090819 <1135314902@qq.com>
2016-04-27 11:03:28 +0800

    信息修改

---
+++ B150908_学生材料/02含批阅意见的实验报告/第02小组：题目名称/源程序及数据文件/car.h
@@ -0,0 +1,38 @@
+/* ①car.h文件的完整内容 */
+#ifndef _car             /*条件编译，防止重复包含的错误*/
+#define _car
+#include <string.h>
+#define NUM 20                /*定义车辆常量，此处可以根据实际需要修改常量值*/
+struct car 
+{
+	char number[10];													//车牌号
+	char name[20];												   //车主姓名
+    char chexing[10];                                          //车型
+    char color[4];                                               //颜色
+	char jiao[5];                                           //缴费状态
+	char time[15];                                           //罚款日期
+	int grade;													      //扣分
+	int money;													     //罚款
+	char weigui[20];                                  		//违规情况
+		int rank;                                          //排列
+} ;
+
+typedef struct car car;
+#define sizeche sizeof(car)      /*车辆记录所需要的内存空间大小*/
+int readche(car che[],int n);       /*读入车辆记录值，车牌号为0或读满规定条数记录时停止*/
+void printche(car  *che , int n);   /*输出所有车辆记录的值*/
+
+int equal(car s1,car s2,int condition);  /*根据condition条件判断两个car类型数据相等否*/
+int larger(car s1,car s2,int condition);  /*根据condition比较car类型数据大小*/
+void reverse(car che[],int n);             /*车辆记录数组元素逆置*/
+
+void calcuRank(car che[],int n);           /*根据扣分多少进行排列，允许有并列名次*/
+
+
+void sortche(car che[],int n,int condition);   /*选择法从小到大排序，按condition所规定的条件*/
+
+int searchche(car che[],int n,car s,int condition,int f[]) ;  /*根据条件找数组中与s相等的各元素*/
+/*下标置于f数组中，设f数组是因为查找结果可能不止一条记录*/
+int insertche(car che[],int n,car s);                   /*向数组中插入一个元素按车牌号有序*/
+int deleteche(car che[],int n,car s);                   /*从数组中删除一个指定车牌号的元素*/
+#endif
